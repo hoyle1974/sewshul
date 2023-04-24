32a8e1e8095d869ce502817a49fc729818b9eb0c
Jack Strohm <hoyle.hoyle@gmail.com>
2023-04-24 11:54:30 -0700

    CI/CD

--- compile.sh
+++ compile.sh
@@ -6,9 +6,15 @@ container=$(buildah from docker.io/library/golang@sha256:d78cd58c598fa1f0c92046f
 echo "Container: $container"
 buildah copy $container "*" .
 buildah config --env GOPATH="" $container
-#buildah run $container go mod download
-#buildah run $container go build .
-buildah run $container make
+buildah run $container cd account
+buildah run $container go mod download
+buildah run $container go build .
+buildah run $container cd ../list
+buildah run $container go mod download
+buildah run $container go build .
+buildah run $container cd ../login
+buildah run $container go mod download
+buildah run $container go build .
 mountpoint=$(buildah mount $container)
 echo "Mountpoint: $mountpoint"
 buildah run $container pwd
