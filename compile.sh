6cf8b581960ccff0c611a5e150e07d84ad8362e9
Jack Strohm <hoyle.hoyle@gmail.com>
2023-04-24 21:08:26 -0700

    CI/CD

--- compile.sh
+++ compile.sh
@@ -7,30 +7,30 @@ echo "Container: $container"
 buildah copy $container "*" .
 buildah config --env GOPATH="" $container
 
-echo " --- Build account ---"
-buildah config --workingdir ./account $container
-buildah run $container go mod download
-buildah run $container go build .
+#echo " --- Build account ---"
+#buildah config --workingdir ./account $container
+#buildah run $container go mod download
+#buildah run $container go build .
 
-echo " --- Build list ---"
-buildah config --workingdir ./list $container
-buildah run $container go mod download
-buildah run $container go build .
+#echo " --- Build list ---"
+#buildah config --workingdir ./list $container
+#buildah run $container go mod download
+#buildah run $container go build .
 
-echo " --- Build login ---"
-buildah config --workingdir ./login $container
-buildah run $container go mod download
-buildah run $container go build .
+#echo " --- Build login ---"
+#buildah config --workingdir ./login $container
+#buildah run $container go mod download
+#buildah run $container go build .
 
-buildah config --workingdir . $container
-mountpoint=$(buildah mount $container)
-echo "Mountpoint: $mountpoint"
-buildah run $container pwd
+#buildah config --workingdir . $container
+#mountpoint=$(buildah mount $container)
+#echo "Mountpoint: $mountpoint"
+#buildah run $container pwd
 buildah run $container find .
-find .
-mkdir sewshul
-cp $mountpoint/go/account ./sewshul/account
-cp $mountpoint/go/list ./sewshul/list
-cp $mountpoint/go/login ./sewshul/login
-buildah unmount $mountpoint
-buildah rm $container
+#find .
+#mkdir sewshul
+#cp $mountpoint/go/account ./sewshul/account
+#cp $mountpoint/go/list ./sewshul/list
+#cp $mountpoint/go/login ./sewshul/login
+#buildah unmount $mountpoint
+#buildah rm $container
