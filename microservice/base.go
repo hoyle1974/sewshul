3024349e86936d441e5ea525ced3c1ef0fed7145
Jack Strohm <hoyle.hoyle@gmail.com>
2023-04-25 23:49:54 -0700

    Change base port

--- microservice/base.go
+++ microservice/base.go
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	port = flag.Int("port", 50051, "The server port")
+	port = flag.Int("port", 8080, "The server port")
 	ip   = flag.String("ip", "0.0.0.0", "address")
 	env  = flag.String("env", "dev", "environment")
 )
